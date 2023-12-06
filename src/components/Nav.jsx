0817b4c264b4382bc0869ca43bd022035b8966ed
genildocs <genildocs@gmail.com>
2023-12-06 03:46:03 -0300

    commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,33 +1,35 @@
-import React, { useState } from 'react';
-import Logo from '../svg/Logo';
-import { motion } from 'framer-motion';
+import React, { useState } from "react";
+import Logo from "../svg/Logo";
+import { motion } from "framer-motion";
+import Facebook from "../svg/Facebook";
+import Twitter from "../svg/Twitter";
 
 export default function Nav() {
   const [open, setOpen] = useState(false);
 
   const navVariants = {
     visible: {
-      position: 'absolute',
-      backgroundColor: 'hsl(229, 31%, 21%)',
-      height: '100vh',
-      width: '100%',
-      margin: '0',
-      padding: '0 2rem',
-      paddingTop: '2.75rem',
-      overflow: 'hidden',
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-around",
+      position: "absolute",
+      backgroundColor: "hsl(229, 31%, 21%)",
+      height: "100vh",
+      width: "100%",
+      overflow: "hidden",
     },
     hidden: {
-      display: 'block',
+      display: "block",
     },
   };
   return (
     <motion.nav
-      className="mt-11 ml-8 mr-8"
       initial={false}
-      animate={open ? 'visible' : 'hidden'}
+      animate={open ? "visible" : "hidden"}
       variants={navVariants}
+      transition={{ type: "Inertia", stiffness: 100 }}
     >
-      <div className="flex justify-between">
+      <div className="flex justify-between mt-11 ml-8 mr-8">
         <div>
           <Logo open={open} />
         </div>
@@ -37,25 +39,25 @@ export default function Nav() {
         >
           <motion.div
             style={{
-              backgroundColor: ['black'],
-              width: ['1.2rem'],
-              height: ['0.3rem'],
-              marginBottom: ['0.3rem'],
+              backgroundColor: ["black"],
+              width: ["1.2rem"],
+              height: ["0.3rem"],
+              marginBottom: ["0.3rem"],
             }}
           ></motion.div>
           <motion.div
             style={{
-              backgroundColor: ['black'],
-              width: ['1.2rem'],
-              height: ['0.3rem'],
-              marginBottom: ['0.3rem'],
+              backgroundColor: ["black"],
+              width: ["1.2rem"],
+              height: ["0.3rem"],
+              marginBottom: ["0.3rem"],
             }}
           ></motion.div>
           <motion.div
             style={{
-              backgroundColor: ['black'],
-              width: ['1.2rem'],
-              height: ['0.3rem'],
+              backgroundColor: ["black"],
+              width: ["1.2rem"],
+              height: ["0.3rem"],
             }}
           ></motion.div>
         </button>
@@ -63,11 +65,11 @@ export default function Nav() {
 
       <motion.div
         className={`${
-          open ? 'block' : 'hidden'
+          open ? "block" : "hidden"
         }  text-white sm:text-black sm:block`}
       >
         <div className=" uppercase text-center text-xl mt-10 ">
-          <ul>
+          <ul className="flex flex-col items-center">
             <a href="#">
               <li className=" p-5">Features</li>
             </a>
@@ -77,12 +79,23 @@ export default function Nav() {
             <a href="#">
               <li className=" p-5">contact</li>
             </a>
+            <a
+              href="#"
+              className="btn text-xl font-medium rounded-md border-2 py-4"
+            >
+              Login
+            </a>
           </ul>
-          <a href="#" className=" text-xl font-medium">
-            Login
-          </a>
         </div>
       </motion.div>
+      <div className="flex justify-center items-center">
+        <div className=" mr-8">
+          <Facebook />
+        </div>
+        <div>
+          <Twitter />
+        </div>
+      </div>
     </motion.nav>
   );
 }
